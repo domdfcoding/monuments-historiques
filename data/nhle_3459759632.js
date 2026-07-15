359398f8b40934227868dd994704915e711c3523
domdfcoding <dominic@davis-foster.co.uk>
2026-07-15 22:52:54 +0000

    Deploying to gh-pages from @ domdfcoding/monuments-historiques@d2a5d5716adc2cce0d42a5c22a0b58bc54a62791 🚀

---
+++ data/nhle_3459759632.js
@@ -0,0 +1,5 @@
+// Lat: 1 — 49, Lng: 2 — 50,
+// Lat,Lng,Number,Name,Grade,ListDate,Link,Notes?, Polygon Points?,
+var monumentsHistoriques3459759632 = [
+[1.9968162411, 49.1631331, "PA95000004", "Croix de cimeti\u00e8re", null, "Wed, 21 Jan 1970", null],
+]
